a7564888b897cdc35a3708726098e6f87e6de38c
chiao0306 <ciaoyuyang@gmail.com>
2026-07-04 17:55:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if uploaded_file:
                 client = DocumentIntelligenceClient(
                     endpoint=AZURE_ENDPOINT,
                     credential=AzureKeyCredential(AZURE_KEY),
-                    api_version="2024-11-30-preview"  # 👈 必須加上這一行，結尾要記得加逗點
+                    api_version="2024-03-30"  # 👈 必須加上這一行，結尾要記得加逗點
                 )
                 
                 # 讀取二進位圖檔
